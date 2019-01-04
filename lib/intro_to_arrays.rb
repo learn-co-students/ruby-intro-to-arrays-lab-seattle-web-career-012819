a58deb5f7f52d602225850ee925446d5922539bc
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-04 11:45:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,5 +10,6 @@ end
 
 def first_element(my_first_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  
+  my_first_element = @taylor_swift[1]
+end
   
